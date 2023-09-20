706bebfb472986623a2ebc1ee6f081eec322ed1e
ratempty <rkdekqls258@naver.com>
2023-09-20 11:00:34 +0900

    mainpage 1.02

--- src/component/page/Project.js
+++ src/component/page/Project.js
@@ -35,30 +35,35 @@ function Project() {
       title: "Mainpage",
       date: "08.20 ~ 08.30",
       ex: "리액트로 만든 개인 포트폴리오 메인 페이지입니다. 구현된 기능으로는 라우트를 사용한 페이지 이동 , axios를 사용한 api 통신으로 날씨정보 받아오기 , formspree를 사용한 이메일 받기 등으로 이루어진 SPA입니다.",
+      href: "",
     },
     {
       id: 1,
       title: "Rate - Market",
       date: "09.01 ~ 09.13",
       ex: "구글의 firebase를 데이터베이스로 사용한 중고물품 거래 사이트입니다. 로그인기능과 글 작성 , 찜하기 , 판매자에게 메세지 보내기 등으로 제작했습니다.",
+      href: "",
     },
     {
       id: 2,
       title: "Caterpillars",
       date: "07/31 ~ 08/05",
       ex: "순수 javascript로 만든 레트로 지렁이 게임입니다. 지렁이를 이동시키면서 생성된 먹이를 먹고 몸 길이를 늘려나가는 2D게임입니다. ",
+      href: "https://ratempty.github.io/game/",
     },
     {
       id: 3,
       title: "Todo-List",
       date: "08.07 ~ 08.19",
       ex: "리액트로 만든 투두리스트입니다. 카테고리 선택 등록 ,카테고리 별 보기 , 해야할 일의 완료 및 고정과 삭제 , 날짜선택 등의 기능을 구현했습니다.",
+      href: "",
     },
     {
       id: 4,
       title: "Asiana airline",
       date: "06.01 ~ 07.30",
       ex: "코딩 시작 2개월차의 첫 프로젝트입니다. 아시아나 항공사의 메인페이지를 리뉴얼하며 반응형으로 제작했습니다. HTML , CSS , JAVASCRIPT 로만 이루어진 사이트로 swiper와 slick 등 라이브러리를 사용해서 배너의 움직임을 구현했습니다.",
+      href: "",
     },
   ];
   const handleScroll = (e) => {
@@ -177,6 +182,9 @@ function Project() {
                 <h4>{item.title}</h4>
                 <p className="makeDay">작업기간 : {item.date}</p>
                 <p className="textEx"> {item.ex}</p>
+                <a href={item.href} target="_blank">
+                  프로젝트 자세히 보기
+                </a>
               </div>
             </li>
           ))}
@@ -185,5 +193,4 @@ function Project() {
     </GlobalStyle>
   );
 }
-
 export default Project;
